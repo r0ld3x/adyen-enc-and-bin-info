80f90886e9f0350fbd9ea8b851f958406f97c2c3
r0ld3x <r0ld3xrahu1@gmail.com>
2022-02-03 12:45:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Bin Info And Adyen Cse Enc Python
+
+> I api for getting bin info and getting encrypted card details for adyen.
+
+![GitHub stars](https://img.shields.io/github/stars/r0ld3x/adyen-enc-and-bin-info)
+![GitHub forks](https://img.shields.io/github/forks/r0ld3x/adyen-enc-and-bin-info)
+[![Maintenance](https://img.shields.io/badge/maintained-yes-green.svg)](https://github.com/r0ld3x/adyen-enc-and-bin-info/commits/master)
+[![Website shields.io](https://img.shields.io/badge/website-up-yellow)](http://varadbhogayata.github.io/)
+[![Ask Me Anything !](https://img.shields.io/badge/ask%20me-Telegram-blue.svg)](https://www.linkedin.com/in/varadbhogayata/)
+[![License](http://img.shields.io/:license-MIT-blue.svg?style=flat-square)](http://badges.mit-license.org)
+
+## Installation
+> Local Installation
+```
+git clone http://www.github.com/r0ld3x/adyen-enc-and-bin-info
+cd adyen-enc-and-bin-info
+pip install -r requirements.txt
+uvicorn index:app
+```
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/r0ld3x/adyen-enc-and-bin-info.git)
+
+
+## Usage
+> website.com = your heroku website name
+
+**BIN INFO**:-
+```curl
+https://website.com/bin/123456
+```
+> Return status code 200 if success else return 404 if bin not found
+
+**ADYEN ENC**:-
+```curl
+https://website.com/adyen
+METHOD = POST
+post_data = {
+  "card": "string", #card 123456789123456
+  "month": "string", #month 01
+  "year": "string", #year 22
+  "cvv": "string", #cvv 123
+  "adyen_version": "string", #adyen key, looks like this: "10001|A2370..."
+  "adyen_key": "string", #_0_25
+}
+```
+> Return enc_card,enc_month,enc_year,enc_cvv
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+#### • MADE By > [Roldex](https://github.com/r0ld3x)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.py
+++ index.py
@@ -2,8 +2,9 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi import FastAPI, HTTPException
 from fastapi import FastAPI, Request
+from fastapi.responses  import RedirectResponse
 from fastapi.responses import JSONResponse
-from mongo_db import db, get_iso
+from bins import mydict, get_bin_info
 import re
 from pydantic import BaseModel
 from py_adyen_encrypt import encryptor
@@ -34,13 +35,6 @@ app = FastAPI(debug=True,
               description=" Feel free to use. made by @roldexverse for roldexverse subscribers.")
 
 
-async def find_bin(id) -> object:
-    find = await db.bins.find_one({'number':int(id)})
-    print(find)
-    if find is None:
-        return None
-    else:
-        return find
 
 
 class UnicornException(Exception):
@@ -57,39 +51,36 @@ async def unicorn_exception_handler(request: Request, exc: UnicornException):
     )
 
 
-@app.get("/bin/{bin}")
-async def bin(bin):
-    price = ''.join(re.findall("\d+", bin))
-    if len(price) == 0:
+@app.get("/bin/{bin1}")
+async def bin(bin1):
+    price = bin1
+    if not price.isdigit():
         raise HTTPException(
             status_code=404, detail="Give me 6 digits Numbers.")
     elif len(price) < 6:
         raise HTTPException(
             status_code=404, detail="Give me 6 numbers atleast.")
     bin = price[:6]
-    bin_data = await find_bin(bin)
-    if bin_data is None:
+    bin_data = get_bin_info(bin)
+    if not bin_data:
         raise HTTPException(status_code=404, detail="Bin Not Found")
     return {
-        'bin': bin_data['number'],
-        'bank': bin_data['bank'],
-        'country_iso': bin_data['country'],
-        'country': get_iso(bin_data['country']),
+        'bin': bin,
+        'bank': bin_data['bank_name'],
+        'country_iso': bin_data['iso'],
+        'country': bin_data['country'],
         'flag': bin_data['flag'],
         'vendor': bin_data['vendor'],
         'type': bin_data['type'],
         'level': bin_data['level'],
+        'prepaid': bin_data['prepaid'],                                                                               
     }
 
 
 @app.get("/")
-async def bin(bin):
-    return {
-        "Made by":  'https://t.me/r0ld3x',
-        'Github': 'https://github.com/r0ld3x',
-        'Telegram': 'https://t.me/RoldexVerse',
-        'Docs': 'https://roldexverseapi.herokuapp.com/docs'
-    }
+async def start():
+    return RedirectResponse("http://www.github.com/r0ld3x/adyen-enc-and-bin-info")
+
 
 class Item(BaseModel):
     card: str
